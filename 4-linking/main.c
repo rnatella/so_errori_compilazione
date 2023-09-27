9c071b83451077043585c360d1fd15ca747b2340
Roberto Natella <roberto.natella@unina.it>
2023-09-27 18:38:18 +0200

    Ridotto numero di iterazioni, compatibili con il tipo int

--- 4-linking/main.c
+++ 4-linking/main.c
@@ -5,7 +5,7 @@ extern int fact(int n);
 int main(void)
 {
   // Call the function to find the factorial of the integers from 1 ... 30
-  for (int i = 1; i <= 30; i++)
+  for (int i = 1; i <= 12; i++)
     printf("fact(%d) = %d\n", i, fact(i));
 
   return 0;
